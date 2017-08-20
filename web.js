2ee45341efc2b5d971556b13688495b247a1f975
Guy <guyguys@gmail.com>
2017-08-20 15:14:04 +0300

    mouse

--- web.js
+++ web.js
@@ -2,11 +2,18 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     const browser = await puppeteer.launch();
-const page = await browser.newPage();
-await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36');
-await page.setViewport({width: 1280, height:768});
-await page.goto('https://web.whatsapp.com/');
-await page.screenshot({path: 'example.png'});
+    const page = await browser.newPage();
+    const mouse = await page.mouse;
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36');
+    await page.setViewport({width: 1280, height: 768});
+    await page.goto('https://web.whatsapp.com/');
+    await page.screenshot({path: 'example2.png'});
 
-browser.close();
+    // await page.waitForSelector('img');
+
+    // await mouse.click(10, 20);
+
+    // console.log('clicked -> ', true);
+
+    // browser.close();
 })();
